7a0cbacf40d5f23649d81d6eda4f700d24607efd
June <June@ADMINIB-SB31VQF>
2015-05-25 13:02:52 +0800

    test

---
+++ src/javaProj001/onlyfortest.java
@@ -0,0 +1,5 @@
+package javaProj001;
+
+public class onlyfortest {
+
+}
---
+++ src/javaProj001/onlyfortest2.java
@@ -0,0 +1,10 @@
+package javaProj001;
+
+public class onlyfortest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
